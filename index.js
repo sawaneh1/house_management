54cb6ad70d8a4dd30a86396fded8e1345abb983b
sawaneh <sawaneh@pop-os.localdomain>
2022-06-22 14:39:27 +0000

    make it better

--- index.js
+++ index.js
@@ -74,26 +74,6 @@ app.listen(PORT, () =>
 // )
 // .catch((error) => console.log(`${error} did not connect`));
 
-// app.use(async (req, res, next) => {
-//   if (req.headers["x-access-token"]) {
-//     const accessToken = req.headers["x-access-token"];
-//     const { userId, exp } = await jwt.verify(
-//       accessToken,
-//       process.env.JWT_SECRET
-//     );
-//     // Check if token has expired
-//     if (exp < Date.now().valueOf() / 1000) {
-//       return res.status(401).json({
-//         error: "JWT token has expired, please login to obtain a new one",
-//       });
-//     }
-//     res.locals.loggedInUser = await User.findById(userId);
-//     next();
-//   } else {
-//     next();
-//   }
-// });
-
 app.use("/", adminRoutes);
 app.use("/", paymentRoutes);
 app.use("/", houseRoutes);
