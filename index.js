684801789d7e113d72f6f38ab675ca4f6c3d0816
sawaneh <sawaneh@pop-os.localdomain>
2022-06-22 12:42:24 +0000

    Purge cache

--- index.js
+++ index.js
@@ -61,18 +61,18 @@ app.use(express.json()); // Used to parse JSON bodies
 app.use(express.urlencoded({ extended: false }));
 app.use("/File", express.static(path.join(__dirname, "File")));
 
-const CONNECTION_URL = process.env.MONGODB_URI;
+// const CONNECTION_URL = process.env.MONGODB_URI;
 const PORT = process.env.PORT || 5000;
 app.use(cors());
 
-mongoose
-  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() =>
-    app.listen(PORT, () =>
-      console.log(`Server Running on Port: http://localhost:${PORT}`)
-    )
-  )
-  .catch((error) => console.log(`${error} did not connect`));
+// mongoose
+//   .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+// .then(() =>
+app.listen(PORT, () =>
+  console.log(`Server Running on Port: http://localhost:${PORT}`)
+);
+// )
+// .catch((error) => console.log(`${error} did not connect`));
 
 // app.use(async (req, res, next) => {
 //   if (req.headers["x-access-token"]) {
